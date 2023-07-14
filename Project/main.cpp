99399ca50aedbfe166543ed0fa63ee59fc1c3385
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 22:25:56 +0100

    DrawingTheBOARD

--- Project/main.cpp
+++ Project/main.cpp
@@ -7,13 +7,14 @@ char myNumbers[9] = {};
 int main() {
     char FirstPerson[100];  // Assuming a maximum length of 100 characters
     printf("Enter your name: ");
-    scanf("%s", FirstPerson);
-    DrawBoard();
+    scanf("%s", FirstPerson);//Getting the person name
+    DrawBoard(); //Calling the function to draw the board
+    //This will be called after each go
 
 }
 
 void DrawBoard() {
-
+//This will be called after each go
     printf("Welcome to Tic Tac Toe!\n");
     printf("You will be X and the computer will be O.\n");
     printf("  %c    | %c  | %c\n ", myNumbers[0], myNumbers[1], myNumbers[2]);
