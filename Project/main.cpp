0b6ff5a62b8330fcbb68874909b13d8e4dfafc6e
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 21:56:58 +0100

    Start

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,7 +3,7 @@
 void DrawBoard();
 
 void DrawBoard(bool b, char i, int marker);
-
+char myNumbers[9] = {};
 int main() {
     char FirstPerson[100];  // Assuming a maximum length of 100 characters
    char Computer[10] = "Computer";
@@ -14,6 +14,7 @@ int main() {
     char SecondMarker = 'O';
     DrawBoard(GameStart, FirstMarker,0);
     GameStart = true;
+    printf("%c",myNumbers[2])
 
 }
 
@@ -28,6 +29,15 @@ void DrawBoard(bool b, char i,int marker) {
         printf("You will be X and the computer will be O.\n");
         return;
     }
-    printf("check");
+    if (marker == 0) {
+        printf("   |   | \n");
+        printf(" %c |   | \n", i);
+        printf("---|---|---\n");
+        printf("   |   | \n");
+        printf("---|---|---\n");
+        printf("   |   | \n");
+        return;
+    }
+
 }
 
