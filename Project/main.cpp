410c836ac20f3b1cb312667d5a2695ffa9942977
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-14 21:46:20 +0100

    Start

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+void DrawBoard();
+
+void DrawBoard(bool b, char i);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char FirstPerson[100];  // Assuming a maximum length of 100 characters
+   char Computer[10] = "Computer";
+   bool GameStart = false;
+    printf("Enter your name: ");
+    scanf("%s", FirstPerson);
+    char FirstMarker = 'X';
+    char SecondMarker = 'O';
+    DrawBoard(GameStart, FirstMarker);
+    GameStart = true;
+
+}
+
+void DrawBoard(bool b, char i) {
+    if (!b) {
+          printf("Welcome to Tic Tac Toe!\n");
+        printf("   |   | \n");
+        printf("---|---|---\n");
+        printf("   |   | \n");
+        printf("---|---|---\n");
+        printf("   |   | \n");
+        printf("You will be X and the computer will be O.\n");
+        return;
+    }
+
+
 }
