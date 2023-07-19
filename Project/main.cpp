9c9efa7d175f6db3e2bd83ff1ee18a991e636733
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-19 20:25:53 +0100

    Done

--- Project/main.cpp
+++ Project/main.cpp
@@ -107,11 +107,9 @@ bool FindWinner(bool Player1) { // This is the function that will check if their
             return true;
         } else if (myNumbers[0] == 'X' && myNumbers[4] == 'X' && myNumbers[8] == 'X') {
             printf("Game Over\n");
-
             return true;
         } else if (myNumbers[2] == 'X' && myNumbers[4] == 'X' && myNumbers[6] == 'X') {
             printf("Game Over\n");
-            printf("Congratulations Player “%s”, YOU ARE THE WINNER! ",Name);
             return true;
         }
     }
